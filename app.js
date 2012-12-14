65451567bd115002a177a529c9c4dcbd7b19026d
Paul Woods <pwoods@ti.com>
2012-12-13 22:49:50 -0600

    changed port to 8080

--- app.js
+++ app.js
@@ -55,6 +55,6 @@ app.configure('development', function(){
 // add all of the routes
 require('./routes')(app);
 
-app.listen(3000);
+app.listen(8080);
 
-console.log('Listening on port 3000');
+console.log('Listening on port 8080');
