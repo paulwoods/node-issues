e1984703ea677b2f1bdb1413e5132a6a8edc7192
Paul Woods <pwoods@ti.com>
2012-12-13 22:47:04 -0600

    Renamed data.jade to index.jade

--- routes.js
+++ routes.js
@@ -7,7 +7,7 @@ var express = require('express')
 module.exports = function(app) {
 
 	app.get('/', function(req, res, next) {
-		res.render('data', {title:''});
+		res.render('index', {title:''});
 	});
 
 }
